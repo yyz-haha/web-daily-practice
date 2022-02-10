27f914fde566ef6cec2b856c3c9a2114682174c6
yyz-haha <2011704409@qq.com>
2022-02-10 21:06:41 +0800

    modify

--- DayA001/String.js
+++ DayA001/String.js
@@ -9,3 +9,30 @@ JavaScript String 对象
 	toLowerCase()、toUpperCase()、trim()、toString()
 	
 **********************************************************************/
+// var str="Hello world, welcome to the universe.";
+// var n=str.indexOf("world");
+// console.log(n);
+
+// var str="Hello world, welcome to the universe.";
+// var n=str.lastIndexOf("el");
+// console.log(n);
+
+// var str="The rain in SPAIN stays mainly in the plain"; 
+// var n=str.match(/ain/g);  //结果是个数组
+// console.log(n);
+// if(n == null) {
+// 	console.log("not matched");
+// } else {
+// 	console.log("matched");
+// }
+
+// var str = "HELLO WORLD";
+// var n = str.charAt(2);
+// console.log(n);
+
+// var str = "HELLO WORLD";
+// var n = str.charCodeAt(0);
+// console.log(n);
+
+// var n = String.fromCharCode(65);
+// console.log(n);
