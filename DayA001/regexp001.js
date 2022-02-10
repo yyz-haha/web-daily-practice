fef0748aa996b330b4733f3e0a282f634e55a542
yyz-haha <2011704409@qq.com>
2022-02-10 14:40:29 +0800

    add DayA001

---
+++ DayA001/regexp001.js
@@ -0,0 +1,40 @@
+/*********************************************************************
+1.字符串查找函数indexOf、search函数使用；
+2.search函数搭配正则表达式使用；
+3.注意indexOf、search函数查找""时会返回0；
+4.&&、||使用；
+**********************************************************************/
+
+// let mstr = "hello";
+// console.log(mstr.indexOf(""));
+// if(mstr.indexOf("") > -1) {
+// 	console.log("find");
+// } else {
+// 	console.log("not find");
+// }
+
+
+// let region = "hello";
+// var re = new RegExp("/" + region + "$" + "|" + "/" + region + "/", 'g');
+// console.log(re);
+// if(mstr.search(re) > -1) {
+// 	console.log("find");
+// } else {
+// 	console.log("not find");
+// }
+
+
+// var mstr = "week";
+// var mstr2 = "day";
+// var res = mstr && mstr2;
+// console.log(res);
+
+// var mstr = "week";
+// var mstr2 = "day";
+// var res = mstr || mstr2;
+// console.log(res);
+
+
+// var re = new RegExp("/hello$world");
+// // var re = /\/hello$world/g;
+// console.log(re);
