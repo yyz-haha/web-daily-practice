4449ff238f5eed2500f03344c3f22e3efe1973d1
yyz-haha <2011704409@qq.com>
2022-05-29 23:11:09 +0800

    add

---
+++ DayA006/test.js
@@ -0,0 +1,14 @@
+$(function(){
+	$('#p1').on('click', function(){
+		$('div').toggle();
+	});
+	
+	// $('div').hover(function() {
+	// 	$(this).addClass('hover');
+	// }, function() {
+	// 	$(this).removeClass('hover');
+	// });
+	
+	window.location.href = "http://www.xxx.com/download?name=file.pdf";
+	
+});
